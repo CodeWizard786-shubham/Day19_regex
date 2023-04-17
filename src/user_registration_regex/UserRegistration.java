d2565ee8afba2382dfd3fb34e3e9c336be29c97c
shubham shirke <shirkeshubham84@gmail.com>
2023-04-17 18:35:05 +0530

    Added: Validate User Email using regex

--- src/user_registration_regex/UserRegistration.java
+++ src/user_registration_regex/UserRegistration.java
@@ -3,13 +3,25 @@ import java.lang.Exception;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public void checkName(String name) throws Exception{
-    String namePattern="^[A-Z][a-z]{2,}$";
-    boolean b= Pattern.matches(namePattern,name);
-    if(!b){
-        throw new RuntimeException();
-    }else{
-        System.out.println("Name correct");
+    public void checkName(String name) throws Exception {
+        String namePattern = "^[A-Z][a-z]{2,}$";
+        boolean b = Pattern.matches(namePattern, name);
+        if (!b) {
+            throw new RuntimeException();
+        } else {
+            System.out.println("Name correct");
+        }
     }
+
+    public void checkEmail(String email) throws Exception {
+        String emailValidation = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+        boolean b = Pattern.matches(emailValidation, email);
+        if (!b) {
+            throw new RuntimeException();
+        } else {
+            System.out.println("Email Correct : " + email);
+        }
+    }
+
     }
 }
--- src/user_registration_regex/UserRegistrationTest.java
+++ src/user_registration_regex/UserRegistrationTest.java
@@ -7,20 +7,28 @@ public class UserRegistrationTest {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         UserRegistration user = new UserRegistration();
-        System.out.println("Enter First name");
+        System.out.println("Enter First name: ");
         String firstName = sc.nextLine();
         try {
             user.checkName(firstName);
         } catch (Exception e) {
             throw new Exception("First Name not correct");
         }
-        System.out.println("Enter First name");
+        System.out.println("Enter Last name: ");
         String lastName = sc.nextLine();
         try {
             user.checkName(lastName);
         } catch (Exception e) {
             throw new Exception("last Name not correct");
         }
+        System.out.println("Enter Email Id: ");
+        String email =sc.nextLine();
+        try {
+            user.checkEmail(email);
+        } catch (Exception e) {
+            throw new Exception("Email not correct");
+        }
+
 
     }
 }
